4b9086f82b9a4f9337a82d6560a8e346049ea7e2
Margaret <maenunezt@gmail.com>
2023-06-30 00:25:41 -0400

    sprint finalizado porque yo lo digo

--- app/src/main/java/com/example/sprintmodulo4/MainActivity.java
+++ app/src/main/java/com/example/sprintmodulo4/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.sprintmodulo4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sprintmodulo4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.infoDeContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(nextPage);
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/example/sprintmodulo4/SecondActivity.java
+++ app/src/main/java/com/example/sprintmodulo4/SecondActivity.java
@@ -2,13 +2,51 @@ package com.example.sprintmodulo4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.sprintmodulo4.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
+    private ActivitySecondBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        binding= ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initListeners();
+
+    }
+
+    private void initListeners() {
+        binding.linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent linkedinIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/gencina2/"));
+                startActivity(linkedinIntent);
+            }
+        });
+
+        binding.whatssap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent whatsappIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.whatsapp.com"));
+                startActivity(whatsappIntent);
+
+            }
+        });
+        binding.correo.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent correoIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gmail.com/"));
+                startActivity(correoIntent);
+            }
+        });
     }
 }
+
+
